db222abb8b9cf23439a9bfde106ab1967466039e
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-05-17 00:22:18 -0700

    Sort commits in order of days.

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -5,34 +5,29 @@ class GithubController < ApplicationController
 
   def statusboard
 
-    # github = Github.new do |config|
-    #   config.endpoint    = 'https://github.company.com/api/v3'
-    #   config.site        = 'https://github.company.com'
-    #   config.oauth_token = '2289fa1f5eb0195a6d631b023ecf2e41d9d388d6'
-    #   config.user        = 'rymanalo'
-    #   config.adapter     = :net_http
-    #   config.ssl         = {:verify => false}
-    # end
-    # github = Github.new oauth_token: '2289fa1f5eb0195a6d631b023ecf2e41d9d388d6'
-
-    # repos = github.repos.commits.all  'rymanalo', 'music_search'
-
-    # file = open('https://api.github.com/users/rymanalo/repos')
-    # repos = JSON.load(file.read)
-
 
     client = Octokit::Client.new(:login => "me", :oauth_token => ENV['GITHUB_TOKEN'])
     repos = client.repositories("rymanalo")
     repo_names = repos.map {|repo| repo['name']}
-    # user2 = Octokit.repo("rymanalo/music_search")
 
 
 
     repo_commits = repo_names.map do |name|
-      "#{name}: #{client.list_commits("rymanalo/#{name}").count}"
+      client.commits("rymanalo/#{name}").map do |sha|
+        (sha['commit']['author']['date']).gsub(/(\A\d*-\d*-\d*)T\d*:\d*:\d*Z/, "\\1")
+      end
     end
 
-    render :json => repo_commits
+    commits = repo_commits.flatten.sort
+    unique_days = commits.uniq
+
+    commits_per_day = unique_days.map do |day|
+      {"title" => day, "value" => (commits.select {|commit| commit == day}).count}
+    end
+
+    datapoints = commits_per_day
+
+    render :json => datapoints
   end
 
 end
